8c63c8ea9c7a6cf94ff74cf801e201b3404b9069
Hirisci <www.alan92@icloud.com>
2022-05-04 11:34:21 +0200

    inserito secondo snack

--- snack-1/js/main.js
+++ snack-1/js/main.js
@@ -2,9 +2,10 @@ const num1 = Number(prompt("Inserisci il primo numero"));
 const num2 = Number(prompt("Inserisci il secondo numero"));
 
 
-if(num1>num2){
-    console.log(`il primo numero é maggiore del secondo`);
-} else {
-    console.log("il secondo numero é maggiore del primo")
-}
+// if(num1>num2){
+//     console.log(`il primo numero é maggiore del secondo`);
+// } else {
+//     console.log("il secondo numero é maggiore del primo")
+// }
 
+num1>num2 ? console.log(`il primo numero é maggiore del secondo`) : console.log("il secondo numero é maggiore del primo");
