c02d1c13c7c3c586de4d2877ba60e5b52f74af73
Hirisci <www.alan92@icloud.com>
2022-05-04 16:24:46 +0200

    add snack 6

--- snack-6/js/main.js
+++ snack-6/js/main.js
@@ -1,24 +1,12 @@
-// Consegna:
-// Scrivi un programma che stampi in console i numeri da 1 a 100.
-// MILESTONE 1
-// Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-// MILESTONE 2
-// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
-// MILESTONE 3
-// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+// Snack 6
+
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 
+let num = Number(prompt("Inserisci un numero"))
 
-const container = document.querySelector(".container");
-
-for(let i = 1; i<=100; i++){
-    let element= document.createElement("div")
-    element.classList.add("box")
-    if(i%3===0) 
-        element.classList.add("fizz")
-    if(i%5===0) 
-        element.classList.add("buzz")
-    element.innerText = i ;
-    container.append(element);
+if(num%2===0){
+    console.log(num);
+} else {
+    console.log(++num);
 }
